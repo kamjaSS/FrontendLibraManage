89944368609262c12752b0ff4365a6f04abbbd60
dany guancha <dany.1701627413@ucaldas.edu.co>
2023-12-14 14:36:54 -0500

    Nuevos cambios para inicio de sesion

--- src/Components/Auth.js
+++ src/Components/Auth.js
@@ -5,6 +5,7 @@ import { Navigate, useLocation } from 'react-router-dom'
 export const setToken = (token, correo) => {
     localStorage.setItem('token', token);
     localStorage.setItem('correo', correo);
+    window.location.reload();
 }
 
 export const fetchToken = () => {
--- src/Components/Fine/FineView.js
+++ src/Components/Fine/FineView.js
@@ -5,79 +5,110 @@ import '../../index.css'
 import PopUpForgivenFine from './PopUpForgivenFine';
 import PopUpPayFine from './PopUpPayFine';
 import PopUpDeleteFine from './PopUpDeleteFine';
+import { decodeToken } from 'react-jwt';
+import { fetchToken } from '../Auth';
 
 const FineView = () => {
-    const [fineData, setFineData] = useState([]);
+  const [fineData, setFineData] = useState([]);
+  const [roles, setRoles] = useState([]);
 
-    const fetch = async () => {
+  const getNameRol = (rolId) => {
+    const role = roles.find((rol) => rol.id === rolId);
+    return role ? role.nombre : 'N/A';
+  };
+  useEffect(() => {
+    const fetchRoles = async () => {
+      try {
+        const response = await api.get('/all_roles/',
+          {
+            headers: { 'Authorization': `Bearer ${fetchToken()}` }
+          });
+        if (response.status === 200) {
+          setRoles(response.data);
+        }
+      } catch (error) {
+        console.error("Error en la respuesta del servidor:", error);
+      }
+    };
+    fetchRoles();
+  }, []);
+  const rolId = parseInt(decodeToken(localStorage.getItem('token')).role, 10);
+  const rol_name = getNameRol(rolId);
+  console.log("rol_name:", rol_name);
+
+  const fetch = async () => {
+    const response = await api.get('/all_finee/');
+    setFineData(response.data);
+  };
+
+  const actualizarMultas = async () => {
+    await api.post('/add_fine_automatically/')
+    fetch();
+  }
+
+  useEffect(() => {
+    actualizarMultas()
+    const fetchData = async () => {
+      try {
         const response = await api.get('/all_finee/');
-        setFineData(response.data);
+        console.log(Array.isArray(response.data));
+        if (Array.isArray(response.data)) {
+          setFineData(response.data);
+          console.log(response.data);
+        } else {
+          console.error("La respuesta de la API no es un array:", response.data);
+        }
+      } catch (error) {
+        console.error("Error fetching authors:", error);
+      }
     };
 
-    const actualizarMultas = async () => {
-       await api.post('/add_fine_automatically/')
-       fetch();
-    }
-    
-    useEffect(() => {
-        actualizarMultas()
-        const fetchData = async () => {
-        try {
-            const response = await api.get('/all_finee/');
-            console.log(Array.isArray(response.data));
-            if (Array.isArray(response.data)) {
-            setFineData(response.data);
-            console.log(response.data);
-            } else {
-            console.error("La respuesta de la API no es un array:", response.data);
-            }
-        } catch (error) {
-            console.error("Error fetching authors:", error);
-        }
-        };
-    
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
 
-    return (
-        <div className='container'>
-        <h2 className='text-center'><strong>Informe</strong></h2>
-  
-        <table className='table table-striped table-bordered table-hover mt-4'>
-          <thead>
-            <tr>
-              <th>id</th>
-              <th>Fecha Pago</th>
-              <th>Valor Deuda</th>
-              <th>Estado</th>
-              <th>ID Prestamo</th>
-              <th>Acciones</th>
-              {/* Agrega más columnas según la estructura de tus datos */}
+
+  return (
+    <div className='container'>
+      <h2 className='text-center'><strong>Informe</strong></h2>
+
+      <table className='table table-striped table-bordered table-hover mt-4'>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Fecha Pago</th>
+            <th>Valor Deuda</th>
+            <th>Estado</th>
+            <th>ID Prestamo</th>
+            <th>Acciones</th>
+            {/* Agrega más columnas según la estructura de tus datos */}
+          </tr>
+        </thead>
+        <tbody>
+          {console.log("multas:", fineData)}
+          {fineData.map(item => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.fechaDePago}</td>
+              <td>{item.valorDeuda}</td>
+              <td>{item.estadoMulta === 0 ? 'Pendiente' : item.estadoMulta === 1 ? 'Pagada' : 'Perdonada'}</td>
+              <td>{item.id_prestamo}</td>
+              <td>
+                <PopUpPayFine multaPay={item} onPay={fetch} />
+                {(rol_name === 'Administrador' || rol_name === 'Bibliotecario') && (
+                  <PopUpForgivenFine multaForgiven={item} onForgiven={fetch} />
+                )}
+                {rol_name === 'Administrador' && (
+                  <PopUpDeleteFine multaDel={item} onDelete={fetch} />
+                )}
+              </td>
+              {/* Agrega más celdas según la estructura de tus datos */}
             </tr>
-          </thead>
-          <tbody>
-            {console.log("multas:", fineData)}
-            {fineData.map(item => (
-              <tr key={item.id}>
-                <td>{item.id}</td>
-                <td>{item.fechaDePago}</td>
-                <td>{item.valorDeuda}</td>
-                <td>{item.estadoMulta === 0 ? 'Pendiente' : item.estadoMulta === 1 ? 'Pagada' : 'Perdonada' }</td>
-                <td>{item.id_prestamo}</td>
-                <td>
-                    <PopUpPayFine multaPay={item} onPay={fetch} />
-                    <PopUpForgivenFine multaForgiven={item} onForgiven={fetch} />
-                    <PopUpDeleteFine multaDel={item} onDelete={fetch} />
-
-                </td>
-                {/* Agrega más celdas según la estructura de tus datos */}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    )
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
 
 }
 
